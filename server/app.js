44f7e0f48735c8c275c1d09f7c1a8446cda9ada7
Joanna Tomassoni <jctomassoni@gmail.com>
2019-12-26 15:57:23 -0600

    add * to app.get path for serving the react static files

--- server/app.js
+++ server/app.js
@@ -12,15 +12,15 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // serve static assets
-const CLIENT_PATH = path.resolve(__dirname, '../build');
+const CLIENT_PATH = path.resolve(__dirname, '../public');
 
 // api routers from router.js
 app.use('/api', router);
 
 // serve react app
 app.use(express.static(CLIENT_PATH));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
