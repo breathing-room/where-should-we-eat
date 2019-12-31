0a4db378f1075afe41d3ac5464be5fd6681b5c40
Joanna Tomassoni <jctomassoni@gmail.com>
2019-12-31 14:36:45 -0600

    run npm run build :

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1000cb760933f9d26ec919f1bc964cf8.js"
+  "/precache-manifest.cbf5d54ee37ce0e14d47349d92718d7b.js"
 );
 
 self.addEventListener('message', (event) => {
